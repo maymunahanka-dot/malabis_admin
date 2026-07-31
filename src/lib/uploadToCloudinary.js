4522120ac1a7f7969be48eb0577d7dec59b44470
TallyAfrica <maymunahanka@gmail.com>
2026-07-31 13:58:11 +0100

    d

--- src/lib/uploadToCloudinary.js
+++ src/lib/uploadToCloudinary.js
@@ -2,6 +2,8 @@ const CLOUD_NAME    = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || dojxfvsaw
 const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET || 'ml_default'
 const FOLDER        = import.meta.env.VITE_CLOUDINARY_UPLOAD_FOLDER || 'img'
 
+console.log('rrrrrrrrrr',CLOUD_NAME, UPLOAD_PRESET, FOLDER);
+
 export async function uploadToCloudinary(file) {
   const fd = new FormData()
   fd.append('file', file)
